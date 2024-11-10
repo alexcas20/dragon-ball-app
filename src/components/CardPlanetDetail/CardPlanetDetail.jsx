cc5b26bf47372eaa04d637ce2ec889e9718a3dbf
alexcas20 <alexcalvillosalas@gmail.com>
2024-11-10 16:50:43 -0600

    fx: responsive carousel to large screen

--- src/components/CardPlanetDetail/CardPlanetDetail.jsx
+++ src/components/CardPlanetDetail/CardPlanetDetail.jsx
@@ -9,7 +9,7 @@ export const CardPlanetDetail = ({ planet }) => {
  
 
   return (
-    <div className="text-slate-200 p-4  md:p-0 flex flex-col gap-4 md:items-center">
+    <div className="text-slate-200 p-4  md:p-0 flex flex-col gap-4 md:items-center ">
       <img
         className="rounded-lg md:rounded-none md:h-[300px] md:w-full md:object-cover md:drop-shadow-xl "
         src={planet.image}
@@ -21,7 +21,7 @@ export const CardPlanetDetail = ({ planet }) => {
       <h3 className="text-slate-100 text-xl text-center uppercase font-bold tracking-[8px] opacity-95 m-4 md:text-4xl md:opacity-80 ">
         {planet.name}
       </h3>
-      <div className="bg-slate-800 p-6 rounded-lg text-center  md:mx-6  ">
+      <div className="bg-slate-800 p-6 rounded-lg text-center  md:mx-6 md:w-[80%]  ">
         <p className="text-slate-100 text-[18px] text-center uppercase font-bold tracking-[5px] opacity-95">
           {planet.description}
         </p>
@@ -34,7 +34,7 @@ export const CardPlanetDetail = ({ planet }) => {
       </h2>
 
       {characters.length >= 1 ? (
-        <div className="mb-24 ">
+        <div className="mb-24 md:w-[500px] ">
           {/*   Carousel characters  */}
           <CarouselCharacter data={characters} isLinked={true} />
          
