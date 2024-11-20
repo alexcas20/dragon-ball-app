cb48d0b7424814e7e5665ade886891c92ff594e2
alexcas20 <alexcalvillosalas@gmail.com>
2024-11-20 13:03:42 -0600

    feat: include react-scroll-to-component

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import { AppRouting } from "./routes/AppRouting";
 import { BottomNavigation } from "./components/BottomNavigation";
 import { useMediaQuery } from "react-responsive";
 import { Footer } from "./pages/Footer/Footer";
+import ScrollToTop from "react-scroll-to-top";
+import { FaChevronUp } from "react-icons/fa6";
 
 function App() {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   return (
     <>
       {isMobile ? <BottomNavigation /> : <NavBar />}
+
       <div className="flex flex-col min-h-screen">
-      <main className="flex-grow bg-slate-900 w-full">
-        <AppRouting />
-       
-      </main>
+        <ScrollToTop
+          smooth
+          style={{ backgroundColor: "#ff8000", padding: "0 7px" }}
+          component={<FaChevronUp size={25} color="#ffe" />}
+        />
+        <main className="flex-grow bg-slate-900 w-full">
+          <AppRouting />
+        </main>
       </div>
-      
+
       <Footer />
     </>
   );
